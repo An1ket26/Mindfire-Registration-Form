af767e69fe9ec56a7dfa1753eb6d7973ac57be4f
Aniket <01aniket26@gmail.com>
2023-07-26 00:14:26 +0530

    practiced web forms

---
+++ c#/webForms/webForms/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webForms.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" AssociatedControlID="TextBox1" runat="server" Text="Enter Your Name : " CssClass="InputLabel" ></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBxChange"  For="Label1" ></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelGender" AssociatedControlID="RbMale" runat="server" Text="Gender :" />
+            <asp:RadioButton ID="RbMale" runat="server" Text="Male" GroupName="Gender" />
+            <asp:RadioButton ID="RbFemale" runat="server" Text="Female" GroupName="Gender" />
+            <asp:RadioButton ID="RbOther" runat="server" Text="Other" GroupName="Gender" />
+            <br /><br />
+            <asp:Calendar ID="FormCalendar" runat="server" AccessKey="c" ShowTodayButton="False" OnSelectionChanged="FormCalendar_SelectionChanged" />
+            <p>Selected Date : <asp:Label ID="Label2" runat="server" Text="" CssClass="InputLabel" ></asp:Label></p>
+
+            <br />
+             <h2>Select Skills</h2>
+            <asp:CheckBox ID="Checkbox1" Text="C#" runat="server" />
+             <asp:CheckBox ID="Checkbox2" Text="ADO.net" runat="server"  />
+             <asp:CheckBox ID="Checkbox3" Text="ASP.net" runat="server"  />
+             <asp:CheckBox ID="Checkbox4" Text="SQL" runat="server"  />
+            <br/><br />
+            
+            <asp:FileUpload ID="File1" runat="server" AllowMultiple="true"/>
+            <asp:Button ID="Button2" runat="server" OnClick="FileUploadButton" Text="Upload"/>
+            <br />
+            <asp:Label ID="Label3" AssociatedControlID="File1" runat="server" Text="Please select a file to upload."></asp:Label><br/>
+            <br/><br/>
+            <asp:Button ID="Submit1" runat="server" Text="Submit" ToolTip="Submit" OnClick="Submit_Click1" style="margin-left: 77px; margin-top: 30px" Width="80px" />
+
+        </div>
+        <br/>
+        <br/>
+
+        <div>
+            <asp:Button ID="Button3" runat="server" OnClick="Download_Click" Text="Download"/>
+        </div>
+        <p>
+            <asp:HyperLink ID="Link1" runat="server" Text="Navigate to Home" NavigateUrl="~/Default.aspx" />
+            <asp:HyperLink ID="Link2" runat="server" Text="Navigate to Google" NavigateUrl="http://www.google.com"/>
+
+        </p>
+    </form>
+</body>
+</html>
---
+++ c#/webForms/webForms/WebForm1.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace webForms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public void TextBxChange(object sender, EventArgs e)
+        {
+            var text = TextBox1.Text;
+            Label1.ToolTip = text;
+        }
+
+        protected void Submit_Click1(object sender, EventArgs e)
+        {
+            
+            var text=TextBox1.Text;
+            Label1.Text = text;
+
+        }
+
+        protected void FormCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            Label2.Text = FormCalendar.SelectedDate.ToString("dd/MM/yyyy");
+        }
+        protected void FileUploadButton(object sender, EventArgs e)
+        {
+            
+            LogRecord("File Coming");
+            LogRecord(File1.PostedFile.ToString());
+            if(File1.PostedFile!=null && (File1.PostedFile.ContentLength>0))
+            {
+                int count = 0;
+                foreach(HttpPostedFile postFile in File1.PostedFiles)
+                {
+                    string fileName = System.IO.Path.GetFileName(postFile.FileName);
+                    LogRecord(fileName);
+                    string saveLocation = Server.MapPath("upload") + "\\" + fileName;
+                    try
+                    {
+                        postFile.SaveAs(saveLocation);
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Label3.Text = "Error :" + ex.Message;
+                    }
+                }
+                if(count>0)
+                {
+                    Label3.Text = count + " Files uploaded successfully";
+                }
+            }
+            else
+            {
+                Label3.Text = "Please select a file to upload.";
+            }
+        }
+        public static void LogRecord(string message)
+        {
+            Console.WriteLine(message);
+            string pathName = Path.Combine("D:\\Projects\\Assignments\\test\\test2.txt");
+            if (File.Exists(pathName))
+            {
+                File.AppendAllText(pathName, message + Environment.NewLine);
+            }
+            else
+            {
+                File.WriteAllText(pathName, message + Environment.NewLine);
+            }
+        }
+
+        protected void Download_Click(object sender, EventArgs e)
+        {
+            string filePath = "D:\\Projects\\Assignments\\test\\test2.txt";
+            FileInfo fileInfo = new FileInfo(filePath);
+            if (fileInfo.Exists)
+            {
+                Response.Clear();
+                Response.AddHeader("Content-Dispostion", "attachment; filename=" + fileInfo.Name);
+                Response.AddHeader("Content-Length",fileInfo.Length.ToString());
+                Response.ContentType="text/plain";
+                Response.Flush();
+                Response.TransmitFile(fileInfo.FullName);
+                Response.End();
+            }
+        }
+    }
+}
